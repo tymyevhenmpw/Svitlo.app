817bd40aeb6606f6c74d8c6cfff90d0e49c682b8
tymyevhenmpw <139116341+tymyevhenmpw@users.noreply.github.com>
2024-05-16 11:59:05 +0300

    added mobile browser theme color

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -17,6 +17,8 @@ export default function RootLayout(
     return (
         <ClerkProvider>
             <html lang="en">
+                <meta name="theme-color" content="#0a3a40"></meta>
+                
                 <body className={' ${inter.className} bg-gradient-to-br from-primary-experimental to-secondary-experimental'}>
                     <div className="w-full flex flex-col justify-center items-center min-h-screen">
                         <h1 className="mb-2 text-heading1-bold text-primary-experimental">Svitlo</h1>
